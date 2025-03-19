27310bef7882317a6581d0b2d5527ec05feaf881
rahulachuz <rahulachuz69@gmail.com>
2025-03-19 21:49:18 +0530

    first commit

---
+++ frontend/src/components/LatestCollection.jsx
@@ -0,0 +1,41 @@
+import React, { useContext, useEffect, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
+import Title from "./Title";
+import Productitem from "./Productitem";
+
+const LatestCollection = () => {
+  const { products } = useContext(ShopContext);
+  const [latestProducts, setLatestProducts] = useState([]);
+
+  useEffect(() => {
+    setLatestProducts(products.slice(0, 10));
+  }, []);
+
+  return (
+    <div className="my-10">
+      <div className="text-center py-8 text-3xl">
+        <Title text1={"LATEST"} text2={"COLLECTION"} />
+        <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
+          You can start selling handbags online on platforms like your website,
+          e-commerce marketplaces like Amazon, Etsy, or eBay, and social media
+          platforms like Instagram and Facebook
+        </p>
+      </div>
+
+      {/* rendering products */}
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+        {latestProducts.map((item, index) => (
+          <Productitem
+            key={index}
+            id={item._id}
+            image={item.image}
+            name={item.name}
+            price={item.price}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default LatestCollection;
